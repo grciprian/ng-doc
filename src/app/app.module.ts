efe10c3318656947240d50561d680f512cbaa3e0
grciprian <ghenceanu.ciprian@gmail.com>
2019-07-01 23:49:01 +0300

    test commit another branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -47,7 +47,9 @@ import { ShippingComponent } from './shipping/shipping.component';
 })
 export class AppModule { }
 
-
+/*
+  test commit another branch
+*/
 /*
 Copyright Google LLC. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that
